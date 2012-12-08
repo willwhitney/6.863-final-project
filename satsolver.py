1c4382ba9057e11dda6883d1512ca9e3ce074e64
Mika Braginsky <mikabr@Ariel.(none)>
2012-12-08 15:47:48 -0500

    smaller test set and TODO

--- satsolver.py
+++ satsolver.py
@@ -1,17 +1,17 @@
 import random
-from conceptnetquerier import *
+from conceptnetQuerier import *
 
-target = 'questions.txt'
+target = 'fewQuestions.txt'
 ansmap = {'a':0,'b':1,'c':2,'d':3,'e':4}
 
 def testSolver(target):
     #given a target question file, read the questions and makes guesses
     #returns the percentage of the questions answered correctly
     file = open(target).read()
-    questions = file.split('\r\n\r\n')
+    questions = file.split('\n\n')
     correct, total = 0.0,0.0
     for q in questions:
-	q = q.split('\r\n')
+	q = q.split('\n')
         stem, opts, ans = q[1], q[2:7], q[7]
         stem = stem.split()
         stem = (stem[0],stem[1])
@@ -29,14 +29,16 @@ def solve(stem,options):
     #get the relationship between every pair of words
     #scores each option's relationship's similarity to the stem's relationship
     #returns the option index of the most similar option
-    stemR = get_relationship(stem)
+    stemR,stemW = get_relationship(*stem)
+    print stem, stemR
     scores = {}
     for opt in options:
-	optR = get_relationship(opt)
+	optR,optW = get_relationship(*opt)
+	print opt, optR
 	sim = scoreSimilarity(stemR,optR)
 	scores[opt] = sim
     best = max(scores,key=lambda k: scores[k])
-    print stem, best
+    print 'My guess is', best, '\n\n'
     guess = options.index(best)
     return guess
 #    return random.randint(0,4)
